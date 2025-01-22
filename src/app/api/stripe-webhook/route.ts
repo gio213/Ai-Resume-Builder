82a53eb0f51e70242e333d8f8793c5fb59cfc037
Giorgi Patsia <gio.patsia@gmail.com>
2025-01-22 17:17:04 +0100

    fixed private metadata for clerk

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -51,9 +51,7 @@ async function handleSessionCompleted(session: Stripe.Checkout.Session) {
   if (!userId) {
     throw new Error("User ID is missing in session metadata");
   }
-  await (
-    await clerkClient()
-  ).users.updateUserMetadata(userId, {
+  (await clerkClient()).users.updateUserMetadata(userId, {
     privateMetadata: {
       stripeCustomerId: session.customer as string,
     },
